cf3e661efb5084b0b88ee3f0df72fad19ab18908
VoidDeck <chengyugoh@gmail.com>
2017-06-01 18:15:28 +0800

    Initial commit w login

---
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/MainActivity.java
@@ -0,0 +1,16 @@
+package com.logcat.example.pc.orbitalproj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
