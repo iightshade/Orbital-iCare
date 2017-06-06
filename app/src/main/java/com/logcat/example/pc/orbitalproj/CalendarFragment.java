9046853f56487ee2be6327c7829c870bfd0c6a46
VoidDeck <chengyugoh@gmail.com>
2017-06-07 00:10:06 +0800

    used alamkanak api for cal. -unsure how to add events/ unsure whether this api is good?

--- app/src/main/java/com/logcat/example/pc/orbitalproj/CalendarFragment.java
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/CalendarFragment.java
@@ -4,20 +4,63 @@ package com.logcat.example.pc.orbitalproj;
  * Created by PC on 6/2/2017.
  */
 
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 
 public class CalendarFragment extends Fragment {
-
-    public CalendarFragment() {
-    }
+    private WeekView mWeekView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View mView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        mWeekView = (WeekView) mView.findViewById(R.id.weekView);
+
+        //set listener for month change
+        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                List<WeekViewEvent> events=new ArrayList<WeekViewEvent>();
+                return events;
+                //TODO: Handle month change (return new list of events)
+            }
+        });
+
+
+        //set listener for event click
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+                //TODO: Handle event click
+            }
+        });
+
+
+        //set event long press listener
+        mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
+            @Override
+            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+                //TODO: Handle event long press
+            }
+        });
+
+
+        //return view
+        return mView;
     }
 }
