7077d73fffeb9f21ba5524151c60e954f2c64e13
Benjamin <iightshade95@gmail.com>
2017-06-07 15:15:08 +0800

    Completed main skeletal interface for medications. Moving on to data.

--- app/src/main/java/com/logcat/example/pc/orbitalproj/MedicationFragment.java
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/MedicationFragment.java
@@ -12,9 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MedicationFragment extends Fragment {
@@ -22,14 +26,32 @@ public class MedicationFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view;
+        view = inflater.inflate(R.layout.fragment_medication,container, false);
+        GridView medicationGridView = (GridView)view.findViewById(R.id.medicationGridView);
+        Medication temp = new Medication("So much", "MEDICATION");
+
+        //Testing interface, to be removed.
+        List<Medication> tempList= new ArrayList<Medication>();
+        tempList.add(temp);
+        MedicationViewAdapter adapter = new MedicationViewAdapter(getActivity(), tempList);
+        medicationGridView.setAdapter(adapter);
+
+        //medicationGridView.setAdapter(new MedicationViewAdapter(this.getActivity()));
+
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_medication, container, false);
-
-        return view;
-
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
 
--- app/src/main/java/com/logcat/example/pc/orbitalproj/MedicationViewAdapter.java
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/MedicationViewAdapter.java
@@ -2,39 +2,70 @@ package com.logcat.example.pc.orbitalproj;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MedicationViewAdapter extends BaseAdapter{
 
     private Activity context;
+    private List<Medication> medicationArrayList;
+    private LayoutInflater layoutInflater;
+
+    public MedicationViewAdapter(Activity context, List<Medication> medicationArrayList){
+        this.context = context;
+        this.medicationArrayList = medicationArrayList;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    public MedicationViewAdapter() {
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return medicationArrayList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return medicationArrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View view;
+        TextView medicationMainTitle;
+        TextView medicationSubTitle;
 
+        if(convertView == null){
 
+            view = layoutInflater.inflate(R.layout.medication_frame, parent, false);
+            view.setTag(R.id.medicationMainTitle, view.findViewById(R.id.medicationMainTitle));
+            view.setTag(R.id.medicationSubTitle, view.findViewById(R.id.medicationSubTitle));
 
+        }else{
+            view = convertView;
+        }
 
+        medicationMainTitle = (TextView)view.getTag(R.id.medicationMainTitle);
+        medicationSubTitle = (TextView)view.getTag(R.id.medicationSubTitle);
 
+        Medication medication = (Medication)getItem(position);
 
-        return null;
+        medicationMainTitle.setText(medication.getMainTitle());
+        medicationSubTitle.setText(medication.getSubTitle());
+
+        return view;
     }
 }
