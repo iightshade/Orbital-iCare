e1d99695909a5195fd926860054b08da3995f047
VoidDeck <chengyugoh@gmail.com>
2017-06-25 20:38:53 +0800

    display events for each medication. But only display onClick. // repeat events not implemented

--- app/src/main/java/com/logcat/example/pc/orbitalproj/CalendarFragment.java
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/CalendarFragment.java
@@ -1,14 +1,23 @@
 package com.logcat.example.pc.orbitalproj;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,11 +27,34 @@ import java.util.List;
 public class CalendarFragment extends Fragment implements MonthLoader.MonthChangeListener, WeekView.EmptyViewClickListener {
 
     private WeekView mWeekView;
-    private ArrayList<WeekViewEvent> mNewEvents;
+    private List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+    private ArrayList<Boolean> medicationDays;
+
+    FirebaseAuth userAuth;
+    FirebaseUser firebaseUser;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference userReference;
+
+    String userId;
+
+    int year;
+    int month;
+    int day=25;
+    int hour=1;
+    int minute;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        userAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseUser = userAuth.getCurrentUser();
+
+        userId = firebaseUser.getUid();
+        userReference = firebaseDatabase.getReference(userId);
+
         // Inflate the layout for this fragment
         View mView = inflater.inflate(R.layout.fragment_calendar, container, false);
         mWeekView = (WeekView) mView.findViewById(R.id.weekView);
@@ -30,111 +62,105 @@ public class CalendarFragment extends Fragment implements MonthLoader.MonthChang
         //set listener for month change
         mWeekView.setMonthChangeListener(this);
 
+        // Set up empty view click listener.
         mWeekView.setEmptyViewClickListener(this);
 
-        // Initially, there will be no events on the week view because the user has not tapped on
-        // it yet.
-        mNewEvents = new ArrayList<WeekViewEvent>();
+        mWeekView.notifyDatasetChanged();
 
         //return view
         return mView;
     }
 
+
     @Override
-    public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+    public List<WeekViewEvent> onMonthChange(final int newYear, final int newMonth) {
 
-        //for loop firebase medication. Run through each medication date
+        List<WeekViewEvent> matchedEvents = new ArrayList<WeekViewEvent>();
 
-            //for loop if repetition
 
-        for (WeekViewEvent event : mNewEvents) {
-            if (event.getEndTime().getTimeInMillis() > startOfMonth.getTimeInMillis() &&
-                    event.getStartTime().getTimeInMillis() < endOfMonth.getTimeInMillis()) {
-                events.add(event);
+        userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+
+
+                            Medication temp = snapshot.getValue(Medication.class);
+
+                            medicationDays=temp.getMedicationDays();
+
+                            for (Integer i=0 ; i<medicationDays.size() ; i++){
+
+                                List<WeekViewEvent> loopEvents = new ArrayList<WeekViewEvent>();
+
+                                if (medicationDays.get(i)){
+
+                                    int tempUse = i + 2;
+
+                                    Calendar startTime = Calendar.getInstance();
+                                    startTime.set(Calendar.DAY_OF_WEEK, tempUse );
+                                    startTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(temp.getMedicationHour()));
+                                    startTime.set(Calendar.MINUTE, Integer.parseInt(temp.getMedicationMinute()));
+                                    startTime.set(Calendar.MONTH, 6 - 1);
+                                    startTime.set(Calendar.YEAR, 2017);
+                                    Calendar endTime = (Calendar) startTime.clone();
+                                    endTime.add(Calendar.HOUR, 1);
+                                    endTime.set(Calendar.MONTH, 6 - 1);
+                                    WeekViewEvent mEvent = new WeekViewEvent(1, temp.getMedicationTitle(), startTime, endTime);
+
+                                    loopEvents.add(mEvent);
+                                    for (WeekViewEvent pEvent : loopEvents) {
+                                        if (eventMatches(pEvent, newYear, newMonth)) {
+                                            events.add(pEvent);
+
+
+                                }
+                            }
+
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+
+                });
+
+
+        for (WeekViewEvent event : events) {
+            if (eventMatches(event, newYear, newMonth)) {
+                matchedEvents.add(event);
             }
         }
 
-        // Get the starting point and ending point of the specific medicine.
-        Calendar startOfEvent = Calendar.getInstance();
-        startOfEvent.set(Calendar.YEAR, startYearEvent);  //year== startyear
-        startOfEvent.set(Calendar.MONTH, startMonthEvent - 1); //month == startmonth
-        startOfEvent.set(Calendar.DATE, 1);
-        startOfEvent.set(Calendar.HOUR_OF_DAY, 0);
-        startOfEvent.set(Calendar.MINUTE, 0);
-        startOfEvent.set(Calendar.SECOND, 0);
-        startOfEvent.set(Calendar.MILLISECOND, 0);
-        Calendar endOfEvent = (Calendar) startOfEvent.clone();
-        endOfEvent.set(Calendar.YEAR,endYearEvent); //end Year== end of event
-        endOfEvent.set(Calendar.MONTH, endMonthEvent); //endMonth== end of event
-        endOfEvent.set(Calendar.DATE,endDateEvent);
-        endOfEvent.set(Calendar.HOUR_OF_DAY, 23);
-        endOfEvent.set(Calendar.MINUTE, 59);
-        endOfEvent.set(Calendar.SECOND, 59);
 
+        return matchedEvents;
 
-
-        // Populate the week view with events.
-        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-
-        Calendar startTime = Calendar.getInstance();
-        startTime.set(Calendar.DATE,16);
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        Calendar endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.MINUTE, 30);
-        endTime.set(Calendar.MONTH, newMonth-1);
-        WeekViewEvent event = new WeekViewEvent(3, "hihi", startTime, endTime);
-        events.add(event);
-
-        return events;
     }
-
 
     @Override
     public void onEmptyViewClicked(Calendar time) {
 
+        /*// Set the new event with duration one hour.
         Calendar endTime = (Calendar) time.clone();
         endTime.add(Calendar.HOUR, 1);
 
         // Create a new event.
-        WeekViewEvent event = new WeekViewEvent(1, "New event", time, endTime);
-        mNewEvents.add(event);
+        WeekViewEvent event = new WeekViewEvent(20, "New event", time, endTime);
+        mNewEvents.add(event);*/
 
         // Refresh the week view. onMonthChange will be called again.
         mWeekView.notifyDatasetChanged();
-
     }
 
-    private ArrayList<WeekViewEvent> getNewEvents(int year, int month) {
 
-        // Get the starting point and ending point of the given month. We need this to find the
-        // events of the given month.
-        Calendar startOfMonth = Calendar.getInstance();
-        startOfMonth.set(Calendar.YEAR, year);
-        startOfMonth.set(Calendar.MONTH, month - 1);
-        startOfMonth.set(Calendar.DAY_OF_MONTH, 1);
-        startOfMonth.set(Calendar.HOUR_OF_DAY, 0);
-        startOfMonth.set(Calendar.MINUTE, 0);
-        startOfMonth.set(Calendar.SECOND, 0);
-        startOfMonth.set(Calendar.MILLISECOND, 0);
-        Calendar endOfMonth = (Calendar) startOfMonth.clone();
-        endOfMonth.set(Calendar.DAY_OF_MONTH, endOfMonth.getMaximum(Calendar.DAY_OF_MONTH));
-        endOfMonth.set(Calendar.HOUR_OF_DAY, 23);
-        endOfMonth.set(Calendar.MINUTE, 59);
-        endOfMonth.set(Calendar.SECOND, 59);
 
-        // Find the events that were added by tapping on empty view and that occurs in the given
-        // time frame.
-        ArrayList<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-        for (WeekViewEvent event : mNewEvents) {
-            if (event.getEndTime().getTimeInMillis() > startOfMonth.getTimeInMillis() &&
-                    event.getStartTime().getTimeInMillis() < endOfMonth.getTimeInMillis()) {
-                events.add(event);
-            }
-        }
-        return events;
+    private boolean eventMatches(WeekViewEvent event, int year, int month) {
+        return (event.getStartTime().get(Calendar.YEAR) == year && event.getStartTime().get(Calendar.MONTH) == month- 1) || (event.getEndTime().get(Calendar.YEAR) == year && event.getEndTime().get(Calendar.MONTH) == month - 1);
     }
+
 
 }
