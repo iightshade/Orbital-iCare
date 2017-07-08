85cbc44f771fa76efaae5d18482ab5835da2b107
VoidDeck <chengyugoh@gmail.com>
2017-07-09 00:09:15 +0800

    same signout button for both google login and password login. TODO: integrate revoke access for google login

--- app/src/main/java/com/logcat/example/pc/orbitalproj/SettingsFragment.java
+++ app/src/main/java/com/logcat/example/pc/orbitalproj/SettingsFragment.java
@@ -17,50 +17,48 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 
 public class SettingsFragment extends Fragment {
 
-    private Button signOut, signOut2;
-    private FirebaseAuth mAuth;
+    private Button signOut, revokeAccess;
     private GoogleApiClient mGoogleApiClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mView=inflater.inflate(R.layout.fragment_settings, container, false);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        signOut2= (Button) mView.findViewById(R.id.button2);
-
-
-
-
-
-
-        signOut2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut2();
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user == null) {
-                    // user auth state is changed - user is null
-                    // launch login activity
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    getActivity().finish();
-                }
-
-            }
-        });
-
-
 
         signOut= (Button) mView.findViewById(R.id.sign_out);
+        revokeAccess= (Button) mView.findViewById(R.id.revoke_access);
+
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String providerId = user.getProviders().get(0);
+
+        if (providerId.equals("google.com")){
+            revokeAccess.setVisibility(View.VISIBLE);
+        }else{
+            revokeAccess.setVisibility(View.GONE);
+        }
+
+
+
+
+
+
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signOut();
+                if (providerId.equals("password")){
+                    signOut();
+                }else{
+                    signOutGoogle();
+                }
+
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user == null) {
                     // user auth state is changed - user is null
@@ -70,6 +68,23 @@ public class SettingsFragment extends Fragment {
                 }
             }
         });
+
+
+        revokeAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                revokeAccess();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                    getActivity().finish();
+                }
+            }
+        });
+
+
 
         return mView;
     }
@@ -97,9 +112,9 @@ public class SettingsFragment extends Fragment {
     }
 
 
-    private void signOut2() {
+    private void signOutGoogle() {
         // Firebase sign out
-        mAuth.signOut();
+        FirebaseAuth.getInstance().signOut();
 
         // Google sign out
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
@@ -111,6 +126,19 @@ public class SettingsFragment extends Fragment {
                     }
                 });
     }
+
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+
+
+
+                    }
+                });
+    }
+
 
 
 
